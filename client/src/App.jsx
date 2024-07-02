55675c15b2e373faa50eb7e444a3334782a13c8c
Dev Hassan <devhassann@gmail.com>
2024-07-02 12:12:32 -0700

    client side initializing complete

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Login } from "./routes";
+
+const App = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/routes.js
@@ -0,0 +1,8 @@
+import Dashboard from "./pages/dashboard";
+import Login from "./pages/login";
+import Users from "./pages/users";
+import Tasks from "./pages/tasks";
+import Trash from "./pages/trash";
+import TaskDetails from "./pages/task-details";
+
+export { Dashboard, Login, Users, Tasks, Trash, TaskDetails };
