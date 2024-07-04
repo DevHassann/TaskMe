6ee5b2a7dd4b724f964dd866449303287fc61b2b
Dev Hassan <devhassann@gmail.com>
2024-07-04 09:36:15 -0700

    login page and app routing completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
-import { Login } from "./routes";
+import { Toaster } from "sonner";
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+
+import { Layout } from "./utils/layout";
+import { Dashboard, Login, TaskDetails, Tasks, Trash, Users } from "./routes";
 
 const App = () => {
   return (
-    <>
-      <Login />
-    </>
+    <main className="w-full min-h-screen bg-[#f3f4f6]">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/completed/:status" element={<Tasks />} />
+          <Route path="/in-progress/:status" element={<Tasks />} />
+          <Route path="/todo/:status" element={<Tasks />} />
+          <Route path="/team" element={<Users />} />
+          <Route path="/trashed" element={<Trash />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
+        </Route>
+
+        <Route path="/log-in" element={<Login />} />
+      </Routes>
+
+      <Toaster richColors />
+    </main>
   );
 };
 
